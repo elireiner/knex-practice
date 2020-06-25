9f1ba43b11f4b7868c4ad6010b582af6f2770f35
Eli Reiners <goelyukim@gmail.com>
2020-06-25 19:39:14 -0400

    Remove unnecessary changes

--- test/shopping-list-service.spec.js
+++ test/shopping-list-service.spec.js
@@ -9,7 +9,7 @@ describe('shoppingListService', () => {
         {
             id: 1,
             name: 'Turnip the Beet',
-            price: Number(0.20),
+            price: '0.20',
             category: 'Lunch',
             checked: true,
             date_added: new Date('2029-01-22T16:28:32.615Z')
@@ -17,7 +17,7 @@ describe('shoppingListService', () => {
         {
             id: 2,
             name: 'Mascarphony',
-            price: Number(1.80),
+            price: '1.80',
             category: 'Lunch',
             checked: true,
             date_added: new Date('2100-05-22T16:28:32.615Z')
@@ -25,7 +25,7 @@ describe('shoppingListService', () => {
         {
             id: 3,
             name: 'Burgatory',
-            price: Number(1.50),
+            price: '1.50',
             category: 'Main',
             checked: false,
             date_added: new Date('1919-12-22T16:28:32.615Z'),
@@ -60,7 +60,7 @@ describe('shoppingListService', () => {
             it('returns the correct data', () => {
                 return ShoppingListService
                     .getAllItems(db)
-                    .then(actual => expect(actual).to.equal(testItems));
+                    .then(actual => expect(actual).to.eql(testItems));
             });
         });
     });
