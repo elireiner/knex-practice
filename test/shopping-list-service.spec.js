736b61532d74e9456f75c27a9c007fb9ca7210b8
Eli Reiners <goelyukim@gmail.com>
2020-07-01 11:06:51 -0400

    Add tests for all service methods

--- test/shopping-list-service.spec.js
+++ test/shopping-list-service.spec.js
@@ -27,7 +27,7 @@ describe('shoppingListService', () => {
             name: 'Burgatory',
             price: '1.50',
             category: 'Main',
-            checked: false,
+            checked: true,
             date_added: new Date('1919-12-22T16:28:32.615Z'),
         }
     ]
@@ -44,25 +44,87 @@ describe('shoppingListService', () => {
 
     after('destroy db connection', () => db.destroy());
 
-    describe('getAllItems', () => {
-        it('returns an empty array', () => {
+    context('when there is data in shopping list', () => {
+        beforeEach('insert test data', () =>
+            db('shopping_list')
+                .insert(testItems)
+        );
+
+        it('getAllItems() returns the correct data', () => {
+            return ShoppingListService
+                .getAllItems(db)
+                .then(actual => expect(actual).to.eql(testItems));
+        });
+
+
+        it('getById()returns the correct item', () => {
+            const testId = 2;
+            const expectedItem = testItems[testId - 1]
+            return ShoppingListService
+                .getById(db, testId)
+                .then(actual => expect(actual).to.eql(expectedItem));
+        });
+
+
+        it('deleteItem() deletes the correct item', () => {
+            const deleteItemId = 1;
+            const expected = testItems.filter(item => { item.id !== deleteItemId })
+            return ShoppingListService
+                .deleteItem(db, deleteItemId)
+                .then(rowsAffected => {
+                    expect(rowsAffected).to.eq(1)
+                    return db('blogful_articles').select('*');
+                })
+                .then(actual => {
+                    expect(actual).to.eql(expected);
+                })
+        });
+
+
+
+        it('updates an item correctly', () => {
+
+            const testItemId = 2;
+            const testItem = testItems.find(itm => itm.id == testItemId)
+            const updatedItem = { ...testItem, checked: false }
+
+            return ShoppingListService
+                .updateItem(db, testItemId, updatedItem)
+                .then(rowAffected => {
+                    expect(rowAffected).to.eql(testItemId - 1)
+                    return db('shopping_list').select('*').where({ id: testItemId }).first();
+                })
+                .then(item => expect(item).to.eql(updatedItem));
+        });
+    });
+
+    context('when there is no data in db', () => {
+        it('getAllItems() returns an empty array', () => {
             return ShoppingListService
                 .getAllItems(db)
                 .then(list => expect(list).to.eql([]))
         })
 
-        context('when there is data in shopping list', () => {
-            beforeEach('insert test data', () => 
-                db('shopping_list')
-                    .insert(testItems)
-            );
+        it(`insertItem() inserts an item and resolves it with an 'id'`, () => {
+            const newItem = {
+                name: 'Test new name name',
+                price: '5.05',
+                date_added: new Date('2020-01-01T00:00:00.000Z'),
+                checked: true,
+                category: 'Lunch',
+            }
 
-            it('returns the correct data', () => {
-                return ShoppingListService
-                    .getAllItems(db)
-                    .then(actual => expect(actual).to.eql(testItems));
-            });
-        });
-    });
+            return ShoppingListService.insertItem(db, newItem)
+                .then(actual => {
+                    expect(actual).to.eql({
+                        id: 1,
+                        name: newItem.name,
+                        price: newItem.price,
+                        date_added: newItem.date_added,
+                        checked: newItem.checked,
+                        category: newItem.category,
+                    })
+                })
+        })
+    })
 })
-
